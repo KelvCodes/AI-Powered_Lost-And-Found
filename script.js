7a0c5ca2e07f188d581f1a2b72eaf73549b16257
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-05-01 15:58:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-   onst geometry = new THREE.TorusKnotGeometry(1, 0.3, 100, 16);
-        const material = new THREE.MeshPhongMaterial({ 
-            color: 0xff0000,
-            specular: 0x6e44ff,
+    specular: 0x6e44ff,
             shininess: 50,
             emissive: 0x330000
         });
