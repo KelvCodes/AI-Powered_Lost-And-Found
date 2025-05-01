acb9cb05b1efd29f05ad920240cd53f4aa8f653a
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-05-01 15:58:39 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-e.adt particleGeometry = new THREE.BufferGeometry();
-        const particleCount = 1000;
-        const positions = new Float32Array(particleCount * 3);
-        for (let i = 0; i < particleCount * 3; i += 3) {
+for (let i = 0; i < particleCount * 3; i += 3) {
             positions[i] = (Math.random() - 0.5) * 30;
             positions[i + 1] = (Math.random() - 0.5) * 30;
             positions[i + 2] = (Math.random() - 0.5) * 30;
