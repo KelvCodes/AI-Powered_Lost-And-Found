ee74d6f47c0068213d30b1a8441bd3c18d69af31
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-05-01 15:57:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-   const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-        const renderer = new THREE.WebGLRenderer({ canvas, alpha: true });
-        renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(window.innerWidth, window.innerHeight);
 
         const geometry = new THREE.TorusKnotGeometry(1, 0.3, 100, 16);
         const material = new THREE.MeshPhongMaterial({ 
