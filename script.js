317d684a481f76e0f5548a56350ea8c5d6040628
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-05-01 15:59:44 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
- animate();
-
-        window.addEventListener('resize', () => {
-            renderer.setSize(window.innerWidth, window.innerHeight);
-            camera.aspect = window.innerWidth / window.innerHeight;
+ animmera.aspect = window.innerWidth / window.innerHeight;
             camera.updateProjectionMatrix();
         });
 
