40b7d0dbd5cf9e417f1c6be7aa4eb4f8cc99b3da
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-06-14 13:11:46 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 
 
-    const canvas = document.getElementById('three-js-background');
-    if (!canvas) return;
-
     // Scene setup
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
