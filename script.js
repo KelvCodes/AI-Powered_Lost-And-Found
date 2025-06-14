c4ab804cb9f126b22c1ffc010ca074e9a8ccd551
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-06-14 13:28:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 
    
-    
-    for (let i = 0; i < count * 3; i += 3) {
-        positions[i] = (Math.random() - 0.5) * 50;
         positions[i + 1] = (Math.random() - 0.5) * 50;
         positions[i + 2] = (Math.random() - 0.5) * 50;
         
